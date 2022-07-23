c0abc5d561ba92338a2eba00b3e02f436fd5f432
Peter Ruse <ruse.peter@gmail.com>
2022-07-23 18:05:20 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Wordle Solver
+
+Cute little program that solves the daily Wordle using Python selenium
---
+++ wordle_solver.py
@@ -0,0 +1,109 @@
+from collections import Counter
+import random
+import requests
+import ast
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class Wordle:
+    wordle_url = 'https://www.nytimes.com/games/wordle/index.html'
+    close_icon_xpath = "//*[local-name()='svg' and @data-testid='icon-close']"
+    keyboard_button_xpath = "//*[local-name()='button' and @data-key='{char}']"
+    game_cell_xpath = "(//*[local-name()='div' and @class='Row-module_row__dEHfN'])[{attempt}]/div[{pos}]/div"
+    letter_count = dict()
+    exact_count = dict()
+    right_letter = [None] * 5
+    letters_not_here = [set() for _ in range(5)]
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+    driver.get(wordle_url)
+
+    def populate_guess_words(self):
+        with open('words.txt', 'r') as f:
+            self.guess_words = f.read().splitlines()
+
+    def start_game(self):
+        self.driver.find_element(By.XPATH, self.close_icon_xpath).click()
+
+    def get_data_state(self, attempt, pos):
+        return self.driver.find_element(
+            By.XPATH, self.game_cell_xpath.format(attempt=attempt, pos=pos)).get_attribute('data-state')
+
+    def press_key(self, char):
+        self.driver.find_element(By.XPATH, self.keyboard_button_xpath.format(char=char)).click()
+
+    def delay(self, seconds):
+        time.sleep(seconds)
+
+    def make_guess(self, attempt):
+        guess_words = []
+        for guess_word in self.guess_words:
+            letter_count = Counter(guess_word)
+            condition_1 = all(letter_count.get(letter, 0) >= self.letter_count[letter] for letter in self.letter_count)
+            condition_2 = all(letter_count.get(letter, 0) == self.letter_count.get(letter, 0) for letter in self.exact_count)
+            condition_3 = all(letter not in letters_not_here for letter, letters_not_here in zip(guess_word, self.letters_not_here))
+            condition_4 = all(letter == right_letter for letter, right_letter in zip(guess_word, self.right_letter) if right_letter)     
+            if condition_1 and condition_2 and condition_3 and condition_4:
+                guess_words.append(guess_word)
+        
+        # of course, update the list of guess words to this narrower list...
+        self.guess_words = guess_words
+
+        guess_word = random.choice(self.guess_words)
+
+        print(f'[ATTEMPT {attempt}] number of guess words to choose from: {len(self.guess_words)}; choosing "{guess_word}"')
+
+        return guess_word
+
+    def enter_guesses(self):
+        attempt = 1
+        while attempt <= 6:
+            guess_word = self.make_guess(attempt)
+
+            # so that the word is typed out at a natural pace, 
+            # put a delay of 0.2 sec between keystrokes...
+            for letter in guess_word:
+                self.press_key(letter)
+                self.delay(0.2)
+
+            # press the enter button to submit the word...
+            self.press_key('↵')
+
+            # sleep 2 seconds while the hints are populated...
+            self.delay(2)
+
+            all_correct = True
+            letter_count = Counter(guess_word)
+            for pos, letter in enumerate(guess_word):
+                data_state = self.get_data_state(attempt, pos + 1)
+                if data_state == 'correct':
+                    self.right_letter[pos] = letter
+                elif data_state == 'present':
+                    self.letters_not_here[pos].add(letter)
+                    all_correct = False
+                elif data_state == 'absent':
+                    letter_count[letter] -= 1
+                    self.exact_count[letter] = True
+                    all_correct = False
+
+            for letter in letter_count:
+                if letter_count[letter] > self.letter_count.get(letter, 0):
+                    self.letter_count[letter] = letter_count[letter]
+
+            if all_correct:
+                break
+
+            attempt += 1
+
+    def play_game(self):
+        self.populate_guess_words()
+        self.start_game()
+        self.enter_guesses()
+        self.delay(3)
+        self.driver.close()
+
+
+if __name__ == '__main__':
+    Wordle().play_game()
