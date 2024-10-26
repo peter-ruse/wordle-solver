811533e73019a376dbff7d657375f77e6c1b78b1
Peter Ruse <ruse.peter@gmail.com>
2024-10-26 17:51:06 -0400

    some improvements

--- wordle_solver.py
+++ wordle_solver.py
@@ -1,11 +1,14 @@
-import os
 import random
 import time
 from collections import Counter
+from pathlib import Path
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -28,38 +31,41 @@ class Wordle:
     attempt = 1
 
     def populate_guess_words(self):
-        with open(f"{os.path.dirname(os.path.abspath(__file__))}/words.txt", "r") as f:
+        with open(Path(__file__).parents[0] / "data" / "words.txt", "r") as f:
             self.guess_words = f.read().splitlines()
 
-    def click_continue_button(self):
-        try:
-            self.driver.find_element(By.XPATH, self.continue_button_xpath).click()
-        except:
-            pass
-
     def click_play_button(self):
-        try:
-            self.driver.find_element(By.XPATH, self.play_button_xpath).click()
-        except:
-            pass
+        WebDriverWait(self.driver, 20).until(
+            EC.presence_of_element_located((By.XPATH, self.play_button_xpath))
+        ).click()
 
     def click_close_intro_button(self):
-        try:
-            self.driver.find_element(By.XPATH, self.close_icon_xpath).click()
-        except:
-            pass
+        WebDriverWait(self.driver, 20).until(
+            EC.presence_of_element_located((By.XPATH, self.close_icon_xpath))
+        ).click()
 
     def start_game(self):
-        self.click_continue_button()
         self.click_play_button()
         self.click_close_intro_button()
 
-    def get_data_state(self, pos):
-        return self.driver.find_element(
-            By.XPATH, self.game_cell_xpath.format(attempt=self.attempt, pos=pos)
-        ).get_attribute("data-state")
+    def get_hints(self):
+        def _get_hints(driver: WebDriver):
+            return [
+                driver.find_element(
+                    By.XPATH, self.game_cell_xpath.format(attempt=self.attempt, pos=pos)
+                ).get_attribute("data-state")
+                for pos in range(1, 6)
+            ]
 
-    def press_key(self, char):
+        def all_useful_hints(driver) -> bool:
+            hints = _get_hints(driver)
+            return all(hint in {"correct", "present", "absent"} for hint in hints)
+
+        WebDriverWait(driver=self.driver, timeout=20).until(all_useful_hints)
+
+        return _get_hints(self.driver)
+
+    def press_key(self, char: str):
         self.driver.find_element(
             By.XPATH, self.keyboard_button_xpath.format(char=char)
         ).click()
@@ -89,7 +95,6 @@ class Wordle:
             if condition_1 and condition_2 and condition_3 and condition_4:
                 guess_words.append(guess_word)
 
-        # of course, update the list of guess words to this narrower list...
         self.guess_words = guess_words
 
         guess_word = random.choice(self.guess_words)
@@ -105,33 +110,31 @@ class Wordle:
         while self.attempt <= 6:
             guess_word = self.make_guess()
 
-            # so that the word is typed out at a natural pace,
-            # put a delay of 0.2 sec between keystrokes...
             for letter in guess_word:
                 self.press_key(letter)
-                time.sleep(0.2)
 
-            # press the enter button to submit the word...
             self.press_key("↵")
 
-            # sleep 2 seconds while the hints are populated...
-            time.sleep(2)
+            hints = self.get_hints()
 
             all_correct = True
             letter_count = Counter(guess_word)
-            for pos, letter in enumerate(guess_word):
-                data_state = self.get_data_state(pos + 1)
-                if data_state == "correct":
-                    self.correct_letter[pos] = letter
-                elif data_state == "present":
-                    self.letters_not_here[pos].add(letter)
-                    all_correct = False
-                elif data_state == "absent":
-                    letter_count[letter] -= 1
-                    self.letters_with_known_count.add(letter)
-                    all_correct = False
+            for pos, letter, hint in zip(range(6), guess_word, hints):
+                match hint:
+                    case "correct":
+                        self.correct_letter[pos] = letter
+                    case "present":
+                        self.letters_not_here[pos].add(letter)
+                        all_correct = False
+                    case "absent":
+                        letter_count[letter] -= 1
+                        self.letters_with_known_count.add(letter)
+                        all_correct = False
+                    case _:
+                        raise ValueError(f"unexpected hint {hint}")
 
             if all_correct:
+                print("guessed the word! exiting game...")
                 break
 
             for letter in letter_count:
